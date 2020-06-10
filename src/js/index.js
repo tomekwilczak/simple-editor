44c72aa82638a2423696437e214cba0e56cf093a
Tomek Wilczak <tomek.wilczak@gmail.com>
2020-06-10 14:06:33 +0200

    Added buttons and localstorage actions (Save, Load, Clear)

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,37 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const saveButton = document.querySelector(".save-button--js");
+const loadButton = document.querySelector(".load-button--js");
+const clearButton = document.querySelector(".clear-button--js");
+const textareaInput = document.querySelector(".textarea--js");
+
+// Sprawdzamy czy localStorage istnieje
+const entry = localStorage.getItem("entry");
+let result = "";
+
+// Jeśli localStorage istnieje, to pod result podpinamy entry (wartośc z localStorage), jeśli nie, to result zostaje pusty
+
+textareaInput.value = result;
+
+const save = () => {
+  console.log("klik w save");
+  localStorage.setItem("entry", textareaInput.value);
+  if (entry) {
+    result = entry;
+  }
+};
+const load = () => {
+  console.log("klik w load");
+  textareaInput.value = localStorage.getItem("entry", { entry });
+};
+
+const clear = () => {
+  textareaInput.value = localStorage.removeItem("entry", { entry });
+  textareaInput.value = "";
+};
+
+loadButton.addEventListener("click", load);
+saveButton.addEventListener("click", save);
+clearButton.addEventListener("click", clear);
